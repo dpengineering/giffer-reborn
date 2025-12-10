4f0d2eb03d4a44d2123281b6f1cdcdb810aac105
cesar-pz <cesar.pz@berkeley.edu>
2025-12-09 20:14:08 -0800

    ex 40 & 41

--- exercises/41/Exercise41/Exercise41.ino
+++ exercises/41/Exercise41/Exercise41.ino
@@ -1,66 +1,23 @@
-//**************ACTIVITIES TO CHECK WHILE GRADING**********BEGIN*********
-//
-// 1.	Ensure that the function is named sequenceAllLEDsForwardBlinkingOff and it is using FOUR for loops
-// 2.	Verify that LEDs blink in the order 2 and 15 off / on 
-// 3.	Make sure that function uses the parameters LEDOnTime, LEDOffTime, and repeatCount
-//
-// ***************ACTIVITIES TO CHECK WHILE GRADING END*********************
-
 void setup()
 {
-  int LEDNum;
-
-  for(LEDNum = 2; LEDNum <= 15; LEDNum++)
+  for(int pin=2; pin<=15; pin++)
   {
-    pinMode(LEDNum, OUTPUT);
+    pinMode(pin,OUTPUT);
   }
 }
 
 void loop()
 {
-  sequenceAllLEDsForwardBlinkingOff(100,0,3);    
-  delay(3000);  
-}
-
-void sequenceAllLEDsForwardBlinkingOff(int onTime, int offTime, int repeatCount) 
-{
-  int count;
-  int pinNumber;
-  int off;
-
-  for(count = 1; count <= repeatCount; count++)
+  int arr[14] = {15,8,7,14,13,6,5,12,11,4,3,10,9,2};
+  
+  for(int count =1; count<=15; count ++) 
   {
-    for (pinNumber = 2; pinNumber <= 15; pinNumber++)
-    {
-      digitalWrite(pinNumber, HIGH);
-      delay(offTime);
-    }
-    for (pinNumber = 2; pinNumber <= 15; pinNumber++)
-    {
-      digitalWrite(pinNumber, LOW);
-      delay(onTime);
-      digitalWrite(pinNumber, HIGH);
-      delay(offTime);
+    for(int i = 0; i < 14; i ++){
+      digitalWrite(arr[i], HIGH);  
+      delay(100);
+      digitalWrite(arr[i], LOW);
+      delay(100); 
     }
   }
-  for (pinNumber = 2; pinNumber <= 15; pinNumber++)
-  {
-    digitalWrite(pinNumber, LOW);
-    delay(offTime);
-  }
+  delay(4000);
 }
-
-// ************************************************BOARD+CONFIGURATION FOOTER BEGIN****************************************************
-//
-// Please do not modify the content of the footer, except for what comes between the triple hashtags (###...###). Thank you!
-// If you're curious, the #%! is to help parse the text for the board and configuration information.
-// In the following line of commented code, please ensure that the board type is correct (either "LED Board" or "KS Board").
-// If you would like additional digital or analog inputs in the exercise, please enter them with the following format:
-// (Keep in mind that the time is in units of milliseconds and the value can range from 0 to 1023.)
-// EXAMPLE 1: "board": {"type":"LED Board", "setup":{"pinKeyframes":[]}}
-// EXAMPLE 2: "board": {"type":"KS Board", "setup":{"pinKeyframes":[{"time":0,"pin":5,"value":0},{"time":2750,"pin":5,"value":260}]}}
-//
-// ACTUAL:#%!"board": {"type":"LED Board", "setup":{"pinKeyframes":[]}}#%!
-//
-// *************************************************BOARD+CONFIGURATION FOOTER END*****************************************************
-
--- exercises/41/Exercise41_StartingPoint/Exercise41_StartingPoint.ino
+++ exercises/41/Exercise41_StartingPoint/Exercise41_StartingPoint.ino
@@ -1,20 +1,84 @@
 void setup()
 {
-  int pinNumber;
-
-  for (pinNumber = 2; pinNumber <= 15; pinNumber++)
+  for(int pin = 2; pin <= 15;pin++)
   {
-    pinMode(pinNumber, OUTPUT);
+    pinMode(pin,OUTPUT);
   }
 }
 
-void loop ()
+//TODO: Clean up the spaghetti code below by using an array and loops
+void loop()
 {
-  yourCustomFunction();
-  delay(3000);
-}
-
-void yourCustomFunction()
-{
-  delay(100);
+  int t = 100;
+  digitalWrite(15, HIGH);  
+  delay(t);
+  digitalWrite(15, LOW);
+  delay(t);
+  
+  digitalWrite(8, HIGH);
+  delay(t);
+  digitalWrite(8, LOW);
+  delay(t);
+  
+  digitalWrite(7, HIGH);
+  delay(t);
+  digitalWrite(7, LOW);
+  delay(t);
+  
+  digitalWrite(14, HIGH);
+  delay(t);
+  digitalWrite(14, LOW);
+  delay(t);
+  
+  digitalWrite(13, HIGH);
+  delay(t);
+  digitalWrite(13, LOW);
+  delay(t);
+  
+  digitalWrite(6, HIGH);
+  delay(t);
+  digitalWrite(6, LOW);
+  delay(t);
+  
+  digitalWrite(5, HIGH);
+  delay(t);
+  digitalWrite(5, LOW);
+  delay(t);
+  
+  digitalWrite(12, HIGH);
+  delay(t);
+  digitalWrite(12, LOW);
+  delay(t);
+  
+  digitalWrite(11, HIGH);
+  delay(t);
+  digitalWrite(11, LOW);
+  delay(t);
+  
+  digitalWrite(4, HIGH);
+  delay(t);
+  digitalWrite(4, LOW);
+  delay(t);
+  
+  digitalWrite(3, HIGH);
+  delay(t);
+  digitalWrite(3, LOW);
+  delay(t);
+  
+  digitalWrite(10, HIGH);
+  delay(t);
+  digitalWrite(10, LOW);
+  delay(t);
+  
+  digitalWrite(9, HIGH);
+  delay(t);
+  digitalWrite(9, LOW);
+  delay(t);
+  
+  digitalWrite(2, HIGH);
+  delay(t);
+  digitalWrite(2, LOW);
+  delay(t);
+    
+  delay(4000);
 }
